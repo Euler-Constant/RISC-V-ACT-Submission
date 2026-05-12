52903825907def1d45bbb766646d16a93d4e5cc0
Euler-Constant <davidorifa39@gmail.com>
2026-05-12 17:24:56 +0100

    error 5 is expected behaviour, trivial parameter configuration

--- uart-config.c
+++ uart-config.c
@@ -9,15 +9,20 @@
 struct termios tty; //tty as naming convention
 
 int main(){
+  //open serial port (in this case, /dev/ttyS0)
   int serial_port = open("/dev/ttyS0", O_RDWR);
 
-  // Check for errors
-  if(serial_port < 0) {
-        printf("Error %i from open: %s\n", errno, strerror(errno));
-        return 1;
-    }
-
+  //check for errors
   if(tcgetattr(serial_port, &tty) != 0) {
     printf("ERROR: %i from tcgetattr: %s\n", errno, strerror(errno));
+    return 1;
   }
+
+  /* CONTROL MODE OPTIONS (C_CFLAG) */
+  tty.c_cflag &= ~PARENB;
+  tty.c_cflag |= PARENB;
+  tty.c_cflag &= ~CSTOPB;
+  tty.c_cflag |= CSTOPB;
+  tty.c_cflag &= ~CSIZE;
+  tty.c_cflag |= CS7; //7-bit data field
 }
