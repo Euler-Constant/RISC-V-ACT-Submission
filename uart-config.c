49700ae70cf8088214a809ec695ce86ed29ded7b
Euler-Constant <davidorifa39@gmail.com>
2026-05-12 14:46:56 +0100

    tcgetattr: Error 5 observed

--- uart-config.c
+++ uart-config.c
@@ -6,6 +6,8 @@
 #include <errno.h> //error handling
 #include <termios.h> //termios API for terminal control
 
+struct termios tty; //tty as naming convention
+
 int main(){
   int serial_port = open("/dev/ttyS0", O_RDWR);
 
@@ -14,4 +16,8 @@ int main(){
         printf("Error %i from open: %s\n", errno, strerror(errno));
         return 1;
     }
+
+  if(tcgetattr(serial_port, &tty) != 0) {
+    printf("ERROR: %i from tcgetattr: %s\n", errno, strerror(errno));
+  }
 }
